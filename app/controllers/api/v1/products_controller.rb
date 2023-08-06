48398cb8972152e922ccdf88e5598bcd5753ad97
jeison poveda <jeisonfpovedac@gmail.com>
2023-08-06 02:17:51 -0500

    fix - se editó la api de productos para no tener que iniciar sesión.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     # app/controllers/api/v1/products_controller.rb
     class ProductsController < ApplicationController
+      skip_before_action :authenticate_user!
+
       def index
         @products = Product.all
         render json: @products
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :orders
 
   namespace :api do
-    resources :products, only: [:index]
+    namespace :v1 do
+      resources :products, only: [:index]
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
