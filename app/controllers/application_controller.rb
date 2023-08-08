e460bf819211644f6ea989f96d7ef812f4f8093c
jeison poveda <jeisonfpovedac@gmail.com>
2023-08-08 02:43:25 -0500

    EDIT - Se editó la lógica para cambiar el logo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_setting
 
-
-
+  def set_setting
+    # Lógica para obtener la configuración, por ejemplo:
+    @setting = Setting.last
+  end
 end
